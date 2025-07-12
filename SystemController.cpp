93a23ad9b5351de54cf6a0ff8a12151ac94a250d
PengyuWan <2960904W@student.gla.ac.uk>
2025-07-13 02:54:10 +0800

    Update SystemController.cpp

--- SystemController.cpp
+++ SystemController.cpp
@@ -436,14 +436,4 @@ void SystemController::alarmMonitoringThread()
 					std::cout << "[SystemController] Alarm triggered!" << std::endl;
 					setBuzzer(true);
 					m_alarmEnabled = false; // Disable alarm after triggering
-				}
-			}
-		}
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
-}
-
-void SystemController::handleError(const std::string &error)
-{
-	std::cerr << "[SystemController] Error: " << error << std::endl;
-}
+		
